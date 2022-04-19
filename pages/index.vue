425bc2f0dd1f4774801be762fc056e81b11c9ccf
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-04-19 18:29:23 +0300

    Fetching data

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,25 @@
 </template>
 
 <script>
+// import axios from 'axios'
 export default {
-  name: 'IndexPage',
+  name: 'index',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+async fetch() {
+const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=&language=en-US&page=1')
+const response = await data.json()
+// console.log(response, "RESULT")
+
+response.results.forEach((movie) =>{
+  this.movies.push(movie)
+})
+
+console.log(this.movies)
+
+}
 }
 </script>
