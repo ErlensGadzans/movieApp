407fdd506dcb67eb2cb9c406449b116bfe150fca
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-04-19 20:28:29 +0300

    Displaying fetching data on main page

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,15 +8,16 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
   plugins: [],
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': [
       'error',
       {
-        ignores: ['default', 'Hero', 'index'],
+        ignores: ['index'],
       },
     ],
   },
+  plugins: ['prettier'],
 }
